934c9d44a26a26c9a83d3f0afa08d0f3535f3ec3
Smartguy <franz.alliod@viacesi.fr>
2019-02-06 15:59:08 +0100

    déploiement fichiers tests + script
    déploiement fichiers de test pour le script + le script et le package de fonction en lui même

---
+++ package/funcmovefile.py
@@ -0,0 +1,16 @@
+# importation de la librairie shutil, ayant des fonctions 
+# pour copier, couper, renommer ou supprimer des fichiers
+import shutil, os, glob
+from os import listdir
+from os.path import isfile, join
+
+def movefile(mypath, filetomove, destination):
+	# si le dossier de destination n'existe pas, le créer
+	#if os.path.isdir(destination) == False:
+    #	os.makedirs(destination)
+	#shutil.commande (Source, Destination)
+	shutil.move(mypath+filetomove, destination)
+ 	#shutil.move(filetomove, 'test\\') working
+
+ 	# On met le programme en pause pour éviter qu'il ne se referme (Windows)
+	# os.system("pause")
---
+++ script.py
@@ -0,0 +1,67 @@
+import os.path, time
+from os import listdir
+from os.path import isfile, join
+
+from package.funcmovefile import movefile
+
+# on utilise ici Windows Security API
+# il faut bien installer au préalable pypiwin32
+import win32api
+import win32con
+import win32security
+
+#le dossier qui sera trié
+mypath = 'test/'
+# on récupère chaque fichier dans ce dossier et on le stocke dans un tableau
+fichiersdansdossier = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+# on récupère le nombre de fichiers dans le dossier mypath
+nombredefichiers = len (fichiersdansdossier)
+# on initialise la variable d'indentation à 0
+i = 0
+#on récupère l'heure actuelle
+timenow = time.time()
+#nombre de secondes dans 2 semaines
+twoweeksago = timenow - 60*60*24*14
+
+# vérifier que la liste n'est pas vide
+if not nombredefichiers:
+	print ("Il n'y a pas de fichiers à trier.")
+	# On met le programme en pause pour éviter qu'il ne se referme (Windows)
+	os.system("pause")
+
+# tant que l'incrémentation est inférieure au nombre de fichiers,
+# récupère et trie les fichiers un par un
+while i < nombredefichiers:
+	
+	# on ouvre une instance du fichier pour récupérer ses informations
+	open (mypath+fichiersdansdossier[i], "w").close ()
+	sd = win32security.GetFileSecurity (mypath+fichiersdansdossier[i], win32security.OWNER_SECURITY_INFORMATION)
+	owner_sid = sd.GetSecurityDescriptorOwner ()
+	
+	# on récupère les valeurs de nom d'utilisateur et de domaine du fichier
+	name, domain, type = win32security.LookupAccountSid (None, owner_sid)
+
+
+	# secondes passées depuis la création du fichier
+	filecreationtime = os.path.getctime(mypath+fichiersdansdossier[i])
+
+	# nombre d'heures avant expiration converti en int pour enlever decimales
+	daysbeforeexpiration = int((filecreationtime - twoweeksago)/3600) 
+
+	print ("\n") # on ajoute une ligne vide pour espacer le tout
+	print ("Fichier suivant :")
+	print (fichiersdansdossier[i])
+	print ("Dernière modification le: %s" % time.ctime(os.path.getmtime(mypath+fichiersdansdossier[i])))
+	print ("Créé le: %s" % time.ctime(os.path.getctime(mypath+fichiersdansdossier[i])) )
+	print ("Le propriétaire du fichier est %s\\%s" % (domain, name))
+	
+	if filecreationtime < twoweeksago :
+		print ("Le fichier est vieux de plus de 2 semaines, il a donc été déplacé vers le dossier d'archivage de son créateur.")
+		
+		# ATTENTION : le dossier de destination DOIT être créer avant de lancer le script pour chaque utilisateur.
+		movefile(mypath, fichiersdansdossier[i], name)
+
+	else :
+		print ("Le fichier est récent et sera archivé dans " + str(daysbeforeexpiration) + " heures.")
+	
+	i += 1
