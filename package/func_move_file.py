fae7fcdcb478861449fee8142678258d990f5b76
Smartguy <franz.alliod@viacesi.fr>
2019-02-13 16:36:28 +0100

    Update func_move_file.py

--- package/func_move_file.py
+++ package/func_move_file.py
@@ -5,12 +5,11 @@ from os import listdir
 from os.path import isfile, join
 
 def movefile(mypath, filetomove, destination):
+	
 	# si le dossier de destination n'existe pas, le créer
 	if os.path.isdir(destination) == False: os.makedirs(destination);
+	
 	# on déplace le fichier de la manière suivante : shutil.move (Source, Destination)
 	shutil.move(filetomove, destination)
 
- 	#shutil.move(filetomove, 'test\\') working
-
- 	# On met le programme en pause pour éviter qu'il ne se referme (Windows)
-	# os.system("pause")
+ 	
