b7b968386678f906a8df0ff02a0d26efb1b7442e
Smartguy <franz.alliod@viacesi.fr>
2019-02-20 15:37:00 +0100

    update script
    ajout structure de contrôle + rajout majuscule entre mots dans variables

--- script.py
+++ script.py
@@ -9,60 +9,67 @@ from package.func_list_files import getListOfFiles
 # il faut bien installer au préalable pypiwin32
 import win32api, win32con, win32security
 
-sys.stdout = open("log.txt", "w")
+now = datetime.datetime.now()
+date = now.strftime("%Y-%m-%d")
+
+try:
+	sys.stdout = open("log-"+date+".txt", "w")
+
+except IOError:
+	print ("Erreur: impossible de trouver le fichier ou les données.")
 
 #le dossier qui sera trié
-mypath = 'directorytosort/'
+myPath = 'directorytosort/'
 # on récupère chaque fichier dans ce dossier et ses sous dossiers et on le stocke dans un tableau
-fichiersdansdossier = getListOfFiles(mypath)
+fichiersDansDossier = getListOfFiles(myPath)
 # on récupère le nombre de fichiers dans le dossier mypath
-nombredefichiers = len (fichiersdansdossier)
+nombreDeFichiers = len (fichiersDansDossier)
 # on initialise la variable d'indentation à 0
 i = 0
 #on récupère l'heure actuelle
-timenow = time.time()
+timeNow = time.time()
 #nombre de secondes dans 2 semaines
-twoweeksago = timenow - 60*60*24*14
+twoWeeksAgo = timeNow - 60*60*24*14
 #on écrit l'heure actuelle pour le fichier log
-now = datetime.datetime.now()
-print (now.strftime("%Y-%m-%d %H:%M"))
+print (date)
+
 # vérifier que la liste n'est pas vide
-if not nombredefichiers:
+if not nombreDeFichiers:
 	print ("Il n'y a pas de fichiers à trier.")
 	# On met le programme en pause pour éviter qu'il ne se referme (Windows)
 	os.system("pause")
 
 # tant que l'incrémentation est inférieure au nombre de fichiers,
 # récupère et trie les fichiers un par un
-while i < nombredefichiers:
+while i < nombreDeFichiers:
 	
 	# on récupère les informations sur le fichier
-	sd = win32security.GetFileSecurity (fichiersdansdossier[i], win32security.OWNER_SECURITY_INFORMATION)
+	sd = win32security.GetFileSecurity (fichiersDansDossier[i], win32security.OWNER_SECURITY_INFORMATION)
 	owner_sid = sd.GetSecurityDescriptorOwner ()
 	
 	# on récupère les valeurs de nom d'utilisateur et de domaine du fichier
 	name, domain, type = win32security.LookupAccountSid (None, owner_sid)
 
 	# secondes passées depuis la création du fichier
-	filecreationtime = os.path.getctime(fichiersdansdossier[i])
+	fileCreationTime = os.path.getctime(fichiersDansDossier[i])
 
 	# nombre d'heures avant expiration converti en int pour enlever decimales
-	daysbeforeexpiration = int((filecreationtime - twoweeksago)/3600) 
+	daysBeforeExpiration = int((fileCreationTime - twoWeeksAgo)/3600) 
 
 	print ("\n") # on ajoute une ligne vide pour espacer le tout
 	print ("Fichier suivant :")
-	print (fichiersdansdossier[i])
-	print ("Dernière modification le: %s" % time.ctime(os.path.getmtime(fichiersdansdossier[i])))
-	print ("Créé le: %s" % time.ctime(os.path.getctime(fichiersdansdossier[i])) )
+	print (fichiersDansDossier[i])
+	print ("Dernière modification le: %s" % time.ctime(os.path.getmtime(fichiersDansDossier[i])))
+	print ("Créé le: %s" % time.ctime(os.path.getctime(fichiersDansDossier[i])) )
 	print ("Le propriétaire du fichier est %s\\%s" % (domain, name))
 	
-	if filecreationtime < twoweeksago :
+	if fileCreationTime < twoWeeksAgo :
 		print ("Le fichier est vieux de plus de 2 semaines, il a donc été déplacé vers le dossier d'archivage de son créateur.")
 		
 		# ATTENTION : le dossier de destination DOIT être créer avant de lancer le script pour chaque utilisateur.
-		movefile(fichiersdansdossier[i], "archives\\"+name)
+		movefile(fichiersDansDossier[i], "archives\\"+name)
 
 	else :
-		print ("Le fichier est récent et sera archivé dans " + str(daysbeforeexpiration) + " heures.")
+		print ("Le fichier est récent et sera archivé dans " + str(daysBeforeExpiration) + " heures.")
 	
 	i += 1
